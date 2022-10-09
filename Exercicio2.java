12e956de60bc5d9ccfc0c01195f9b84ecb82d032
belabenevenuto <isabelagoncalves04@hotmail.com>
2022-10-09 16:58:56 -0300

    adiciona exercicio2

---
+++ Exercicio2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        double numero;
+        Scanner teclado = new Scanner(System.in);
+        numero = teclado.nextDouble();
+        System.out.print("Seu numero é " + numero);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Exercicio2
 Repositório destinado a solução do exercício 2 da lista sequencial de java
+
+RA: 322125103 - ISABELA GONÇALVES CARDOSO
